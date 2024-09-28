ccb89f27f1b293a8077cb03720e7367c949f3f01
Simeon Kummer <simeon@hitthecode.de>
2024-09-28 10:20:41 +0200

    fixed directory problem

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ fs.writeFile("eula.txt", "eula=true", (error) => {
     if (error) {
         throw new Error(error)
     }
-    childProcess.spawnSync("sh", ["-c", "java -jar ./server/leaf-1.20.4.jar"], {stdio: "inherit", stdin: "inherit"})
+    childProcess.spawnSync("sh", ["-c", "cd server && java -jar ./leaf-1.20.4.jar"], {stdio: "inherit", stdin: "inherit"})
 })
